1e1560a6451f255665d17008bf7f71386f30fc06
pamsy <pamelaorline@gmail.com>
2022-10-10 12:38:26 +0100

    image creation is ok

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export POSTGRES_PASSWORD=albuquerque
+export POSTGRES_HOST=instancedbprojet3.cu59icumc0nj.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=pamsydatabase
+export AWS_BUCKET=pamsy-140872405465-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
