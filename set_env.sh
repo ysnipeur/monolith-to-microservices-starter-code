6bcc83246661a3142e6903b2b78879fcdbd0df79
yvan takoumbo <yvansnippeur@gmail.com>
2022-11-20 09:04:01 +0100

    Code deployed

--- set_env.sh
+++ set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 #rien a dire
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=albuquerque
+export POSTGRES_PASSWORD=postgres2022
 export POSTGRES_HOST=instancedbprojet3.cu59icumc0nj.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=pamsydatabase
-export AWS_BUCKET=pamsy-140872405465-dev
+export POSTGRES_DB=udagramDB
+export AWS_BUCKET=yvan-udagram-bucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
